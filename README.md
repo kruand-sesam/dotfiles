da5e605d66918df0ce70162268621a8ca0a4dd19
Andreas Krutnes <andreas.krutnes@sesam.io>
2022-04-28 14:48:54 +0200

    Added symlinks for config files

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 Git:
-
 - Lag private key: .ssh/github.private
 - Legg til i .ssh/config:
     - `IdentityFile ~/.ssh/github.private`
 - Test med ssh -T git@github.com
 
 Terminal Setup:
-
 - ZSH
 - Oh My ZSH
 - p10k plugin
---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# @TODO:
+# Create install script with ZSH+plugins
+
+
+dir=$(pwd)
+
+rm -f -- $HOME/.bash_aliases
+rm -f -- $HOME/.p10k.zsh
+rm -f -- $HOME/.zshrc
+
+ln -s $dir/.zshrc $HOME/.zshrc
+ln -s $dir/.bash_aliases $HOME/.bash_aliases
+ln -s $dir/.bash_aliases $HOME/.p10k.zsh
